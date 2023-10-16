3c76f0c97cf82297463d7642173cd361d0079a62
dimka <dmitryellison@gmail.com>
2023-10-16 12:46:02 +0300

    -refactoring

--- face_recognize/recognizer.py
+++ face_recognize/recognizer.py
@@ -17,14 +17,15 @@ class Recognizer:
         self.face_encoder = torchvision.models.resnet50().to(self.device)
 
         self.face_detector = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False)
-        self._load_weights(self.face_detector, "fasterrcnn_resnet50_fpn.pth")
         self.face_detector.to(device)
+        self._load_weights(path_to_weights)
 
-    @staticmethod
-    def _load_weights(model, path):
-        face_encoder_weights = torch.load(os.path.join(path,
-                                                       'face_encoder.pt'))
-        model.load_state_dict(face_encoder_weights)
+    def _load_weights(self, path):
+        face_encoder_weights = torch.load(os.path.join(path, 'face_encoder.pt'))
+        face_detector_weights = torch.load(os.path.join(path, 'fasterrcnn_resnet50_fpn.pth'))
+
+        self.face_encoder.load_state_dict(face_encoder_weights)
+        self.face_detector.load_state_dict(face_detector_weights)
 
     def recognize(self, image: np.ndarray):
         image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
